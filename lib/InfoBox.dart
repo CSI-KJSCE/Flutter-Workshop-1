bf8b3adfab11c15a1e8fc69cd0b910554b79e005
manan <mrpandya2017@gmail.com>
2020-07-03 23:09:55 +0530

    UI updated

--- lib/InfoBox.dart
+++ lib/InfoBox.dart
@@ -1,79 +1,81 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class InfoBox  extends StatelessWidget {
-  String text1;
-  IconData icon;
-  Color colour;
-  double fontSize1;
-  double height1;
-  double width1;
-  InfoBox(this.text1,this.icon,this.colour,this.fontSize1,this.height1,this.width1);
+
+class InfoBox extends StatelessWidget {
+  final String text1;
+  final icon;
+  final Color colour;
+  final double fontSize1;
+  final double height1;
+  final double width1;
+  InfoBox(this.text1, this.icon, this.colour, this.fontSize1, this.height1,
+      this.width1);
   @override
   Widget build(BuildContext context) {
-    return  Align(
-              alignment: Alignment.center,
-              child: Container(
-                height: height1,
-                width: width1,
-                margin: EdgeInsets.only(top: 6),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: colour,
+    return Align(
+      alignment: Alignment.center,
+      child: Container(
+        height: height1,
+        width: width1,
+        margin: EdgeInsets.only(top: 6),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: colour,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
+                  BoxShadow(
+                      offset: Offset(2.0, 3.0),
+                      blurRadius: 4.0,
+                      spreadRadius: 1.0,
+                      color: Color(0xff000000)),
+                  BoxShadow(
+                      offset: Offset(-2.0, -3.0),
+                      blurRadius: 4.0,
+                      spreadRadius: 1.0,
+                      color: Color(0xfff0f0f0))
+                ]),
+                child: RawMaterialButton(
+                  onPressed: () {},
+                  elevation: 0.0,
+                  fillColor: colour,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Icon(
+                      icon,
+                      color: Colors.white,
+                      size: 20,
                     ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                        height: 40,
-                        width: 40,
-                        decoration:
-                            BoxDecoration(shape: BoxShape.circle, boxShadow: [
-                          BoxShadow(
-                            offset: Offset(3.0, 3.0),
-                            blurRadius: 5.0,
-                            spreadRadius: 2.0,
-                          ),
-                          BoxShadow(
-                              offset: Offset(-2.0, -2.0),
-                              blurRadius: 5.0,
-                              spreadRadius: 1.0,
-                              color: Colors.white)
-                        ]),
-                        child: RawMaterialButton(
-                          onPressed: () {},
-                          elevation: 2.0,
-                          fillColor: colour,
-                          child: Align(
-                            alignment: Alignment.center,
-                            child:Icon(
-                            icon,
-                            color: Colors.white,
-                            size: 20,
-                          ), 
-                          ), 
-                          padding: EdgeInsets.all(10.0),
-                          shape: CircleBorder(),
-                        )),
-                    Container(
-                      height: 50,
-                      width: 290,
-                      margin: EdgeInsets.only(left: 10),
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(7)),
-                      ),
-                      child: Text(
-                        text1,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            //fontFamily: 'Montserrat',
-                            fontSize: fontSize1,
-                            color: Colors.white),
-                      ),
-                    )
-                  ],
-                ),
+                  ),
+                  padding: EdgeInsets.all(10.0),
+                  shape: CircleBorder(),
+                )),
+            Container(
+              height: 50,
+              width: 290,
+              padding: EdgeInsets.only(left: 15),
+              margin: EdgeInsets.only(left: 10),
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(7)),
               ),
-            );
+              child: Text(
+                text1,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    //fontFamily: 'Montserrat',
+                    fontSize: fontSize1,
+                    color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:introapp/InfoBox.dart';
+
+BoxDecoration titledecoration = BoxDecoration(
+    border: Border(
+        bottom: BorderSide(color: Colors.white, style: BorderStyle.solid)));
 
 void main() {
   runApp(MyApp());
@@ -9,186 +15,201 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.orange[900],
-        ),
         body: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Align(
-               alignment: Alignment.topCenter,
-               child: Container(
-                 width: double.infinity,
-                 height: 150.0,
-                 decoration: BoxDecoration(
-                   color: Colors.white,
-                   
-                 ),
-                 alignment: Alignment.center,
-                 padding: EdgeInsets.all(0),
-                 child: Stack(
-                   children: <Widget>[
-                     Align(
-                       alignment: Alignment.topCenter,
-                       child: Container(
-                         width: double.infinity,
-                         height: 100,
-                         decoration: BoxDecoration(
-                          
-                           gradient: LinearGradient(
-                           begin: Alignment.topCenter,
-                           end: Alignment.bottomCenter,
-                           colors: [Colors.orange[900], Colors.yellow[600]]),
-                           
-                         ),
-
-                       ),
-                     ),
-                     Align(
-                       alignment: Alignment.bottomCenter,
-                       child: Container(
-                         height: 110,
-                         width: 110,
-                         decoration: BoxDecoration(
-                           shape: BoxShape.circle,
-                            gradient: LinearGradient(
-                           begin: Alignment.topCenter,
-                           end: Alignment.bottomCenter,
-                           colors: [ Colors.yellow[600],Colors.orange[900]]),
-                          
-                         ),
-                       ),
-                     ),
-                     Align(
-                       alignment: Alignment.bottomCenter,
-                       child: Container(
-                         height: 100,
-                         width: 100,
-                         margin: EdgeInsets.only(bottom:5),
-                         decoration: BoxDecoration(
-                           shape: BoxShape.circle,
-                           image: DecorationImage(
-                             image: AssetImage("assets/photo.jpg"),
-                             fit: BoxFit.fill,
-                           ),
-                          
-                         ),
-                       ),
-                     ),
-                   ],
-                 ),
-               ),
-             ),
-            
-             Align(
-               alignment: Alignment.center,
-               child: Text("Aditya Tawade",
-               style: TextStyle(
-                            
-                            
-                            fontSize: 35,
-                            
-                            color: Colors.orange[700]),
-               ),
-             ),
-             Align(
-               alignment: Alignment.center,
-               child:Container(
-               height: 140,
-               width:400,
-               margin: EdgeInsets.only(top: 10),
+              alignment: Alignment.topCenter,
+              child: Container(
+                width: double.infinity,
+                margin: EdgeInsets.all(10),
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Colors.grey[700] ),
-               child: Column(
-                 children: <Widget>[
+                    color: Color(0xff303030),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 2,
+                          spreadRadius: 1,
+                          offset: Offset(0, 2))
+                    ],
+                    borderRadius: BorderRadius.circular(20)),
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(20),
+                child: Stack(
+                  children: <Widget>[
                     Align(
-                        alignment: Alignment.topLeft,
-                        child: Container(
-                      width: 50,
-                      height: 30,
-                      margin: EdgeInsets.only(top:5,left:10),
-                      child: Text("Bio:",
-                      style: TextStyle(
-                            //fontFamily: 'Montserrat',
-                            fontSize: 25,
-                            color: Colors.white),
-                      
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        width: double.infinity,
+                        height: 150,
                       ),
                     ),
-
-                    ),
-                    
-                         Container(
-                      width: double.infinity,
-                      height: 100,
-                      margin: EdgeInsets.only(top:5,left:10,right:10),
-                      child: Text("Hi There! this is supposed to be my Bio and write whatever I like",
-                      style: TextStyle(
-                            
-                            fontSize: 20,
-                            color: Colors.white),
-                      
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        height: 110,
+                        width: 110,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                        ),
                       ),
                     ),
-                    
-                 ],
-               ),
-             ), 
-             ),
-             Align(
-               alignment: Alignment.center,
-               child: Container(
-                 height:210,
-                 width:400,
-                  margin: EdgeInsets.only(top: 8),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        height: 100,
+                        width: 100,
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: AssetImage("assets/photo.jpg"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 30),
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.symmetric(vertical: 10),
+                              child: Text(
+                                "Aditya Tawade",
+                                style: TextStyle(
+                                    fontFamily: "Sans",
+                                    fontSize: 35,
+                                    color: Colors.white),
+                              ),
+                            ),
+                            Container(
+                                margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                                child: Text("CS undergrad",
+                                    style: TextStyle(
+                                        fontSize: 15, color: Colors.white)))
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+//                height: 140,
+                width: 400,
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.symmetric(vertical: 10),
                 decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Colors.orange[700] ),
-                    child: Column(
-                      children: <Widget>[
-                        Align(
-                        alignment: Alignment.topLeft,
-                        child: Container(
-                      width: 150,
-                      height: 30,
-                      margin: EdgeInsets.only(top:5,left:10),
-                      child: Text("Contacts:",
-                      style: TextStyle(
-                            
-                            fontSize: 20,
-                            color: Colors.white),
-                      
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.grey[700],
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 3,
+                          spreadRadius: 0.5,
+                          offset: Offset(0, 2))
+                    ]),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        width: double.infinity,
+                        height: 30,
+                        decoration: titledecoration,
+                        margin: EdgeInsets.only(top: 5, left: 10),
+                        child: Text(
+                          "About Me:",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+//                              fontFamily: 'Montserrat',
+                              fontSize: 25,
+                              color: Colors.white),
+                        ),
                       ),
                     ),
-
+                    Container(
+                      width: double.infinity,
+                      height: 140,
+                      padding: EdgeInsets.all(20),
+//                      margin: EdgeInsets.only(top: 5, left: 10, right: 10),
+                      child: Center(
+                        child: Text(
+                          "Hi There! this is supposed to be my Bio and write whatever I like",
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ),
                     ),
-                    InfoBox("1234567890", Icons.phone,Colors.orange[900],15,50,380),
-            
-            InfoBox("rohanajm@gmail.com", Icons.email,Colors.orange[900],15,50,380),
-            InfoBox("rohanajm@gmail.com", Icons.email,Colors.orange[900],15,50,380),
-                      ],
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.orange[800],
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 3,
+                          spreadRadius: 0.5,
+                          offset: Offset(0, 2))
+                    ]),
+                child: Column(
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Container(
+                        width: double.infinity,
+                        height: 30,
+                        decoration: titledecoration,
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        margin: EdgeInsets.only(top: 5, left: 10, bottom: 10),
+                        child: Text(
+                          "Contact Me :",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
                     ),
-               ),
-             ),
-             
-             
-           
+                    InfoBox("1234567890", Icons.phone, Colors.orange[800], 15,
+                        50, 380),
+                    InfoBox("rohanajm@gmail.com", Icons.email,
+                        Colors.orange[800], 15, 50, 380),
+                    InfoBox("rohanajm7", FontAwesomeIcons.github,
+                        Colors.orange[800], 15, 50, 380),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
